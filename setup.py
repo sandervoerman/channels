ebc29fbc06c4fe7864df52bf1f0277f68a8d0ff3
Sander Voerman <sander@savoerman.nl>
2018-11-05 02:48:28 +0100

    Add setup script

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="sav.channels",
+    version="0.1",
+    author="Sander Voerman",
+    author_email="sander@savoerman.nl",
+    description="Iterable stream between coroutines",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/sandervoerman/channels",
+    packages=[],
+    py_modules=['sav.channels'],
+    python_requires='>=3.7',
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
